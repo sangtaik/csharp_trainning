e95726497c726a32d9b1d0b256587c4fa1c5d7bf
sangtaik <bluestruggle@naver.com>
2022-07-08 17:42:52 +0900

    src: thread test add

--- server_training/Program.cs
+++ server_training/Program.cs
@@ -9,18 +9,54 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Multithreading_and_Multitasking {
-    class Program {
-        static void Main(string[] age) {
-            Work w = new Work();
-            Console.WriteLine("Main Thread Start");
 
-            ThreadStart s = w.Count;
-            Thread thread1 = new Thread(s);
-            thread1.Start();
+namespace MultithreadingTutorial
+{
 
-            Console.WriteLine("Main Thread Ends");
-            Console.ReadKey();
+    class Program
+    {
+        static void Main(string[] age)
+        {
+
+            int userInput = 0;
+            do
+            {
+                userInput = DisplayMenu();
+                ThreadTutorial t1 = new ThreadTutorial();
+                if (userInput == 0) { System.Environment.Exit(1);}
+                if (userInput == 1) { t1.Run1(); continue;}
+                if (userInput == 2) { t1.Run2(); continue;}
+                if (userInput == 3) { t1.Run3(); continue;}
+                if (userInput == 4) { t1.Run4(); continue;}
+                if (userInput == 5) { t1.Run5(); continue;}
+                if (userInput == 6) { t1.Run6(); continue;}
+                if (userInput == 7) { t1.Run7(); continue;}
+                if (userInput == 8) { t1.Run8(); continue;}
+                if (userInput == 9) { t1.Run9(); continue;}
+                if (userInput == 10) { t1.Run10(); continue;}
+                if (userInput == 11) { t1.Run11(); continue;}
+                if (userInput == 12) { t1.Run12(); continue;}
+                if (userInput == 13) { akshay.Run(); continue;}
+
+
+            } while (userInput != 4);
+
+            static int DisplayMenu()
+            {
+                Console.Clear();
+                while (true)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine();
+                    Console.WriteLine("1. thread one, 2. thread two, 3. thread join, 4. thread sleep, 5. sleep + join, 6. join");
+                    Console.WriteLine("0. exit");
+                    int result;
+                    if (Int32.TryParse(Console.ReadLine(), out result))
+                        return result;
+                    else
+                        Console.WriteLine("Please enter a number");
+                }
+            }
         }
     }
 }
--- server_training/Work.cs
+++ server_training/Work.cs
@@ -5,15 +5,321 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Multithreading_and_Multitasking {
-    class Work {
-        public void Count() {
+namespace MultithreadingTutorial
+{
+    class Work
+    {
+        public void Count()
+        {
             Console.WriteLine("Thread 1 Start");
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < 10; i++)
+            {
                 Console.WriteLine(i);
             }
             Console.WriteLine("Thread 1 End");
         }
 
+        public void CountSleep()
+        {
+            Console.WriteLine("Thread 1 Start");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("CountSleep" + i);
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Thread 1 End");
+        }
+
+
+        public void Alpabets()
+        {
+            Console.WriteLine("Thread 2 Start");
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                Console.WriteLine(c);
+            }
+            Console.WriteLine("Thread 2 End");
+        }
+
+        public int[] iArray = new int[20];
+
+        public void CollectData()
+        {
+            Console.WriteLine("Thread 3 Start");
+            for (int i = 0; i < 20; i++)
+            {
+                iArray[i] = i + 1;
+                Console.Write(",");
+                Thread.Sleep(500);
+            }
+            Console.WriteLine("Thread 3 End");
+        }
+
+    }
+
+
+    class ThreadTutorial
+    {
+        public void Run1()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s = w.Count;
+            Thread thread1 = new Thread(s);
+            thread1.Start();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run2()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s = w.Count;
+            Thread thread1 = new Thread(s);
+            thread1.Start();
+
+            thread1.Join();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run3()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s = w.Count;
+            Thread thread1 = new Thread(s);
+            thread1.Start();
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run4()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s = w.Count;
+            Thread thread1 = new Thread(s);
+            thread1.Start();
+            thread1.Join();
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run5()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.CountSleep;
+            Thread thread1 = new Thread(s1);
+            thread1.Start();
+
+            // Thread.Sleep(1000);
+            // thread1.Join();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+        // join 시점이 어떻게 되는지 확인
+        public void Run6()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.CountSleep;
+            Thread thread1 = new Thread(s1);
+            thread1.Start();
+
+            // Thread.Sleep(1000);
+            thread1.Join();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run7()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.CountSleep;
+            Thread thread1 = new Thread(s1);
+            thread1.Start();
+
+            Thread.Sleep(1000);
+
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run8()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.CountSleep;
+            Thread thread1 = new Thread(s1);
+            thread1.Start();
+            thread1.Join();
+
+            Thread.Sleep(1000);
+
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run9()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.Count;
+            ThreadStart s2 = w.Alpabets;
+            Thread thread1 = new Thread(s1);
+            Thread thread2 = new Thread(s2);
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run10()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.Count;
+            ThreadStart s2 = w.Alpabets;
+            Thread thread1 = new Thread(s1);
+            Thread thread2 = new Thread(s2);
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run11()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.Count;
+            ThreadStart s2 = w.Alpabets;
+            Thread thread1 = new Thread(s1);
+            Thread thread2 = new Thread(s2);
+            thread1.Start();
+            thread2.Start();
+
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+        public void Run12()
+        {
+            Work w = new Work();
+            Console.WriteLine("Main Thread Start");
+
+            ThreadStart s1 = w.Count;
+            ThreadStart s2 = w.Alpabets;
+            Thread thread1 = new Thread(s1);
+            Thread thread2 = new Thread(s2);
+            thread1.Start();
+            thread2.Start();
+
+            Thread.Sleep(1000);
+            thread2.Join();
+
+            Console.WriteLine("Main Thread Ends");
+            Console.ReadKey();
+        }
+
+    }
+}
+
+//참조 : https://www.c-sharpcorner.com/UploadFile/1d42da/1/
+static class akshay
+{
+    static Thread th1;
+    static void ChildThread()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch (Exception)
+        {
+            try
+            {
+                Console.WriteLine("1 thread are going to sleep mode");
+                Thread.Sleep(5000);
+                Console.WriteLine("1 now thread is out of sleep");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+
+    static void ChildThread2()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch (Exception)
+        {
+            try
+            {
+                Console.WriteLine("2 thread are going to sleep mode");
+                Thread.Sleep(2000);
+                Console.WriteLine("2 now thread is out of sleep");
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+    static void Console_CancelKeyPress(Object sender, ConsoleCancelEventArgs e)
+    {
+        Console.WriteLine("aborting");
+        if (th1 != null)
+        {
+            th1.Abort();
+            th1.Join();
+        }
+    }
+
+    public static void Run()
+    {
+        // CTRL + C를 누르면 Abort 호출
+        Console.CancelKeyPress += new ConsoleCancelEventHandler(Console_CancelKeyPress);
+        th1 = new Thread(ChildThread);
+        th1.Start();
+        th1.Join();
+        Console.Read();
     }
 }
